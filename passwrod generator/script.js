b88b0c39589b96a12430842f4d9d5e17d9a0f3b8
cassie_c <cassie.nightingale.13.13@gmail.com>
2022-05-29 17:04:21 -0500

    change clipboardEl

--- passwrod generator/script.js
+++ passwrod generator/script.js
@@ -5,40 +5,28 @@ const lowercaseEl = document.getElementById('lowercase')
 const numbersEl = document.getElementById('numbers')
 const symbolsEl = document.getElementById('symbols')
 const generateEl = document.getElementById('generate')
-const clipboardEl = document.getElementById('clipbard')
+const clipboardEl = document.getElementById('clipboard')
 
 const randomFunc = {
     lower: getRandomLower,
-    upper: getRandomNumber,
+    upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
 }
 
-// clipboardEl.addEventListener('click', () => {
-//     const textarea = document.createElement('textarea')
-//     const password = resultEl.innerText
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea')
+    const password = resultEl.innerText
 
-//     if(!password) { return }
+    if(!password) { return }
 
-//     textarea.value = password
-//     document.body.appendChild(textarea)
-//     textarea.select()
-//     document.execCommand('copy')
-//     textarea.remove()
-//     alert('Password copied to clipboard!')
-// })
-
-clipboardEl.addEventListener("click", () => {
-    copyPassword(resultEl.innerText);
+    textarea.value = password
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
+    alert('Password copied to clipboard!')
 })
- 
-async function copyPassword(password) {
-    try {
-        await navigator.clipboard.writeText(password)
-    } catch(error) {
-        console.error("Error in copying text: ", err)
-    }
-}
 
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value
@@ -53,7 +41,7 @@ generateEl.addEventListener('click', () => {
 function generatePassword(lower, upper, number, symbol, length) {
     let generatedPassword = ''
     const typesCount = lower + upper + number + symbol
-    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item) [0])
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0])
     
     if(typesCount === 0) {
         return ''
